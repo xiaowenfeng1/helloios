e3f0ca8962349a323cddd7299ac7454e16b22511
Your Name <Your email>
2016-03-30 21:59:30 -0700

    helloiso homework with screenshots

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Xiaowen Feng on 3/30/16.
+//  Copyright © 2016 Xiaowen Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
